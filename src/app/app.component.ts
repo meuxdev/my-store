87ae081733451d23bc0876ded3889d52d32b8df1
Alejandro Andrade <meuxdev@gmail.com>
2022-06-16 23:03:26 -0500

    String interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-store';
+  // all attributes should be public
+  // All default attributes are public
+  name: string = 'Alejandro Andrade';
+  age: number = 24;
+  imgUrl = 'http://qnimate.com/wp-content/uploads/2014/03/images2.jpg';
 }
