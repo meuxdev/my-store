31e71930bc7950f21159abea61caeff2d8281494
Alejandro Andrade <meuxdev@gmail.com>
2022-06-16 23:24:05 -0500

    Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,12 @@ export class AppComponent {
   name: string = 'Alejandro Andrade';
   age: number = 24;
   imgUrl = 'http://qnimate.com/wp-content/uploads/2014/03/images2.jpg';
+  btnIsDisable = true;
+  nameInput = 'input random name';
+
+  person = {
+    name: 'Alejandro',
+    age: 24,
+    avatar: 'https://source.unsplash.com/random',
+  };
 }
