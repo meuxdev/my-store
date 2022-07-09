1c28e53fb6ac0301acd42e7a88f02ce6a9fb70a0
Alejandro Andrade <meuxdev@gmail.com>
2022-07-09 02:59:59 -0500

    Input, Outputs and EventEmitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,115 +8,11 @@ import { Product } from "./product.model";
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-store';
-  // all attributes should be public
-  // All default attributes are public
-  name: string = 'Alejandro Andrade';
-  age: number = 24;
-  imgUrl = 'http://qnimate.com/wp-content/uploads/2014/03/images2.jpg';
-  btnIsDisable = true;
-  nameInput = 'input random name';
 
-  widthImg = 10;
+  imgParent: string = "https://cdn.document360.io/da52b302-22aa-4a71-9908-ba18e68ffee7/Images/Documentation/Screenshot from 2022-04-05 22-42-58.png";
+  // imgParent: string = "";
 
-  box = {
-    width: 100,
-    height: 100,
-    background: 'red'
-  };
-
-  person = {
-    name: 'Alejandro',
-    age: 24,
-    avatar: 'https://source.unsplash.com/random',
-  };
-
-
-  register = {
-    name: '',
-    email: '',
-    password: '',
-  }
-
-  ComponentData = {
-    progressBarVal: 43,
-    progressBarId: 'ProgressUniqueId',
-    buttonText: 'Random Button',
-    buttonIsDisabled: true,
-    labelTitle: 'Progress Bar:',
-  };
-
-  // shoppingList: string[] | number[] = ['Tomatoes', 'Carrots', 'Cereal', 'Meat'];
-  // shoppingList: any = ['Tomatoes', 'Carrots', 'Cereal', 'Meat'];
-  shoppingList: string[] = ['Tomatoes', 'Carrots', 'Cereal', 'Meat'];
-  newProduct = "";
-
-  products: Product[] = [
-    {
-      name: 'EL mejor juguete',
-      price: 565,
-      image: './assets/images/toy.jpg',
-      category: 'all',
-    },
-    {
-      name: 'Bicicleta casi nueva',
-      price: 356,
-      image: './assets/images/bike.jpg'
-    },
-    {
-      name: 'Colleción de albumnes',
-      price: 34,
-      image: './assets/images/album.jpg'
-    },
-    {
-      name: 'Mis libros',
-      price: 23,
-      image: './assets/images/books.jpg'
-    },
-    {
-      name: 'Casa para perro',
-      price: 34,
-      image: './assets/images/house.jpg'
-    },
-    {
-      name: 'Gafas',
-      price: 3434,
-      image: './assets/images/glasses.jpg'
-    }
-  ];
-
-
-
-  // en el componente no puede ser privado si va a ser
-  // usando en app.html
-  toggleButton() {
-    this.btnIsDisable = !this.btnIsDisable;
-  }
-
-  increaseAge() {
-    this.person.age += 1;
-  }
-
-  onScroll(event: Event) {
-    const element = event.target as HTMLElement;
-    console.log(element.scrollTop);
-  }
-
-  changeName(event: Event) {
-    const element = event.target as HTMLInputElement;
-    this.person.name = element.value;
-  }
-
-  addProductToList(){
-    this.shoppingList.push(this.newProduct);
-    this.newProduct = "";
-  }
-
-  deleteProduct(index: number) {
-    this.shoppingList.splice(index, 1);
-  }
-
-  onRegister() {
-    console.log(this.register);
+  onLoaded(img: string){
+    console.log("Log Padre", img);
   }
 }
--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImgComponent implements OnInit {
 
+  @Input() img: string = "Initial Value";
+
+  @Input("name") firstName: string = "";
+
+  @Input("hello") set alertHello(firstName: string) {
+    alert(`Hello ${firstName}`);
+  }
+
+  @Output() loaded = new EventEmitter<string>();
+
+  readonly imgDefault: string = "./assets/images/bike.jpg"
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  imgError() {
+    this.img = this.imgDefault;
+  }
+
+  imgLoaded() {
+    console.log("Loaded Successfully.");
+    this.loaded.emit(this.img);
   }
 
 }
