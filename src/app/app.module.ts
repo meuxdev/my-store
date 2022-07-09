4f1ef03b967c352a87292e675ef242782a25dd48
Alejandro Andrade <meuxdev@gmail.com>
2022-07-09 02:24:27 -0500

    Creating component demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImgComponent } from './components/img/img.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ImgComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
