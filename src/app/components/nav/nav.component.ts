49cb99fe29ebb74e9fa361e36df1d136f96f24e8
Alejandro Andrade <meuxdev@gmail.com>
2022-07-11 02:09:46 -0500

    Adding the side bar. Interactive styles

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements OnInit {
+  activeSideMenu: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  sideMenuClick() {
+    // alert('Close side bar');
+    this.activeSideMenu = !this.activeSideMenu;
   }
 
+  ngOnInit(): void {}
 }
