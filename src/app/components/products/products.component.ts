51e8c1b3d146b9f47cb89b034ffc01525e793926
Alejandro Andrade <meuxdev@gmail.com>
2022-07-11 02:24:39 -0500

    Adding the shopping cart functionality

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from "../../models/product.model";
+import { IProduct } from '../../models/product.model';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
-
 export class ProductsComponent implements OnInit {
 
   products: IProduct[] = [
@@ -20,37 +19,47 @@ export class ProductsComponent implements OnInit {
       id: 2,
       name: 'Bicicleta casi nueva',
       price: 356,
-      img: './assets/images/bike.jpg'
+      img: './assets/images/bike.jpg',
     },
     {
       id: 3,
       name: 'Colleción de albumnes',
       price: 34,
-      img: './assets/images/album.jpg'
+      img: './assets/images/album.jpg',
     },
     {
       id: 4,
       name: 'Mis libros',
       price: 23,
-      img: './assets/images/books.jpg'
+      img: './assets/images/books.jpg',
     },
     {
       id: 5,
       name: 'Casa para perro',
       price: 34,
-      img: './assets/images/house.jpg'
+      img: './assets/images/house.jpg',
     },
     {
       id: 6,
       name: 'Gafas',
       price: 3434,
-      img: './assets/images/glasses.jpg'
-    }
-  ]
+      img: './assets/images/glasses.jpg',
+    },
+  ];
 
-  constructor() { }
+  total!: number;
 
-  ngOnInit(): void {
+  myShoppingCart: IProduct[] = [];
+
+  constructor() {
+    this.total = 0;
   }
 
+  ngOnInit(): void {}
+
+  onAddToCart(product: IProduct) {
+    this.myShoppingCart.push(product);
+    // this.total = this.myShoppingCart.reduce((sum, item) => sum + item.price, 0);
+    this.total += product.price;
+  }
 }
