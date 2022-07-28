c6e34fb29eb0d2442ee3d152034bbf5a92322c2c
Alejandro Andrade <meuxdev@gmail.com>
2022-07-28 00:44:20 -0500

    Adding 404 for page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,6 +17,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
