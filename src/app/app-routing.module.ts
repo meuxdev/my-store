bffcac25a979af67d63d77067d3bc00f6b9a21e6
Alejandro Andrade <meuxdev@gmail.com>
2022-07-29 10:59:06 -0500

    Lazy loading of the component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ import { CategoryComponent } from './website/pages/category/category.component';
 import { MycartComponent } from './website/pages/mycart/mycart.component';
 import { LoginComponent } from './website/pages/login/login.component';
 import { RegisterComponent } from './website/pages/register/register.component';
-import { RecoveryComponent } from './website/pages/recovery/recovery.component';
 import { ProfileComponent } from './website/pages/profile/profile.component';
 import { ProductDetailComponent } from './website/pages/product-detail/product-detail.component';
 import { LayoutComponent } from './website/components/layout/layout.component';
@@ -25,6 +24,10 @@ const routes: Routes = [
       { path: 'profile', component: ProfileComponent },
       { path: 'product/:id', component: ProductDetailComponent },
     ],
+  },
+  {
+    path: 'cms',
+    loadChildren: () => import('./cms/cms.module').then((m) => m.CmsModule),
   },
   { path: '**', component: NotFoundComponent },
 ];
---
+++ src/app/cms/cms-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { GridComponent } from '@cms/pages/grid/grid.component';
+import { TasksComponent } from '@cms/pages/tasks/tasks.component';
+import { LayoutComponent } from '@cms/components/layout/layout.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: 'grid', pathMatch: 'full' },
+      { path: 'grid', component: GridComponent },
+      { path: 'task', component: TasksComponent },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class CmsRoutingModule {}
